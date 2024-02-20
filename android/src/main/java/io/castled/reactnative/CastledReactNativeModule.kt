8a6d8c8ed6f59636a85fc6bb43d03c1e4cd604b6
Antony Joe Mathew <antonyjoemathew@gmail.com>
2024-02-20 13:47:59 +0530

    chore: added required methods for the native emitter

--- android/src/main/java/io/castled/reactnative/CastledReactNativeModule.kt
+++ android/src/main/java/io/castled/reactnative/CastledReactNativeModule.kt
@@ -6,12 +6,9 @@ import android.app.Application
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
-import com.facebook.react.bridge.WritableNativeMap
-import com.facebook.react.modules.core.DeviceEventManagerModule
 import io.castled.android.notifications.CastledNotifications
 import io.castled.android.notifications.CastledUserAttributes
 import io.castled.android.notifications.push.models.PushTokenType
-import io.castled.reactnative.consts.CastledEmitterKeys
 import io.castled.reactnative.consts.ConfigKeys
 import io.castled.reactnative.extensions.toCastledConfigs
 import io.castled.reactnative.extensions.toMap
@@ -20,7 +17,9 @@ import io.castled.reactnative.listeners.CastledReactNativePushNotificationListen
 class CastledReactNativeModule internal constructor(context: ReactApplicationContext) :
   CastledReactNativeModuleSpec(context) {
 
-  private val pushListener by lazy { CastledReactNativePushNotificationListener.getInstance(context) }
+  private val pushListener by lazy {
+    CastledReactNativePushNotificationListener.getInstance(context)
+  }
 
   override fun getName(): String {
     return NAME
@@ -55,7 +54,6 @@ class CastledReactNativeModule internal constructor(context: ReactApplicationCon
   @ReactMethod
   override fun logCustomAppEvent(eventName: String, eventParams: ReadableMap?) {
     CastledNotifications.logCustomAppEvent(reactApplicationContext, eventName, eventParams.toMap())
-
   }
 
   @ReactMethod
@@ -63,57 +61,22 @@ class CastledReactNativeModule internal constructor(context: ReactApplicationCon
     val attrs = CastledUserAttributes()
     attrs.setAttributes(attributes.toMap())
     CastledNotifications.setUserAttributes(reactApplicationContext, attrs)
-    sendPushEvent()
   }
 
   companion object {
     const val NAME = "RTNCastledNotifications"
   }
 
-  fun sendPushEvent() {
-    val notificaiton = HashMap<String, Any>()
-    notificaiton.put("title", "Android title")
-    notificaiton.put("body", "contentText")
-//    val finalNotificaito = HashMap<String, HashMap<String, Any>>()
-//    finalNotificaito.put("notificaiton", notificaiton)
-
-    val data = WritableNativeMap().apply {
-      putString("title", "Androdi title")
-      putString("body", "body")
-      putString("summary_text", "summary_text")
-    }
-
-    val finalNotificaito = WritableNativeMap()
-    finalNotificaito.putMap("notification", data)
-    println("sendPushEvent-----------$finalNotificaito")
-    reactApplicationContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
-      .emit(CastledEmitterKeys.PUSH_NOTIFICATION_CLICKED.value, finalNotificaito)
-
-
-  }
-
-  @ReactMethod
-  fun startObserving() {
-    println("startObserving-----------")
-
-    // Implement startObserving functionality
-  }
-
-  @ReactMethod
-  fun stopObserving() {
-    println("stopObserving-----------")
-
-    // Implement stopObserving functionality
-  }
 
   @ReactMethod
   fun addListener(eventType: String?) {
-    eventType?.let {
-    }
+    // Don't Delete: Required for React built in Event Emitter Calls.
+
   }
 
   @ReactMethod
   fun removeListeners(count: Double) {
+    // Don't Delete: Required for React built in Event Emitter Calls.
 
   }
 }
